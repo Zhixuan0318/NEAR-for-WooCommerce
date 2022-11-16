e5c1d61e14f9ddab4d6ed6d39348809cc28a4540
Tan Zhi Xuan <69501009+Zhixuan0318@users.noreply.github.com>
2022-11-17 01:46:26 +0800

    Add files via upload

---
+++ js/admin.js
@@ -0,0 +1,16 @@
+jQuery.noConflict();
+
+window.NEAR_Data = NEAR_vars;
+
+(function( $ ) {
+
+    $(function() {
+
+        window.NEAR_Data = NEAR_vars
+
+
+    });
+
+})(jQuery);
+
+// Other code using $ as an alias to the other library
